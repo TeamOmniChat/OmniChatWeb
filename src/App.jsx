ff7e711ee930c49463419b7d79f76426f7bb3f89
Justin Sun <i@jsun969.cn>
2022-03-11 22:49:15 +0800

    :art: rename all react components to `.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { themeOptions as dark } from "./themes/dark";
 import { themeOptions as light } from "./themes/light";
 
 function App() {
-  const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
   return (
     <ThemeProvider theme={createTheme(isDark ? dark : light)}>
       <CssBaseline />
